dc94183b8f76d9b44a7ef81f006f9bbb095d4879
jay_virt <white.jay.jesse@gmail.com>
2021-12-02 21:29:37 +0200

    day1 initial

---
+++ day1/main.js
@@ -0,0 +1,16 @@
+const getData = require("./script.js");
+
+let data = getData("\n\n");
+
+let reg = /\n/ig;
+
+for (let x = 0; x< data.length; x++) {
+    data[x] = data[x].replace(reg, " ").split(" ")
+    let newEntry = {};
+    for (let y = 0; y < data[x].length;y++){
+        let line = data[x][y].split(":");
+        newEntry[line[0]] = line[1]
+    }
+    newEntry['keys'] = Object.keys(newEntry);
+    data[x] = newEntry;
+}
